41906dddb9ec400781dd44c541c61f251de974b9
Dhruv Garg <dhruv@Dhruvs-MacBook-Air.local>
2023-08-15 21:30:48 +0530

    Header completed with conditions

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,27 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
 // routing
-import {BrowserRouter} from "react-router-dom"
+import { BrowserRouter } from "react-router-dom";
 // to wrap up with redux(provides the state to components)
-import { Provider } from "react-redux"
-import { store } from './store';
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { Provider } from "react-redux";
+import { store } from "./store";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <Provider store={store}>
-      {/* need to mention the state to be implemented */}
-      <App/>
-    </Provider>
+      <Provider store={store}>
+        {/* need to mention the state to be implemented */}
+        <App />
+      </Provider>
     </BrowserRouter>
-
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,28 +1,28 @@
 // will contain all data(states)
-import {configureStore, createSlice} from "@reduxjs/toolkit"
+import { configureStore, createSlice } from "@reduxjs/toolkit";
 
 const authSlice = createSlice({
-    name : "auth", 
-    initialState: {
-        isLoggedIn: false
-    }, 
-    // reducer function
-    reducers : {
-        // action creators
-        login(state) {
-            state.isLoggedIn = true
-        }, 
-        logout(state) {
-            state.isLoggedIn = false
-        }
-    }
-})
+  name: "auth",
+  initialState: {
+    isLoggedIn: false,
+  },
+  // reducer function
+  reducers: {
+    // action creators
+    login(state) {
+      state.isLoggedIn = true;
+    },
+    logout(state) {
+      state.isLoggedIn = false;
+    },
+  },
+});
 
 // need to export action creators as well
-export const authActions= authSlice.actions
+export const authActions = authSlice.actions;
 
 // exporting the reducer function of store which will handle state of the redux
 
 export const store = configureStore({
-    reducer : authSlice.reducer
-})
+  reducer: authSlice.reducer,
+});
