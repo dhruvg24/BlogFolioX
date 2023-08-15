3fa1d2360893d654396ddf73a98672bfd37c7019
Dhruv Garg <dhruv@Dhruvs-MacBook-Air.local>
2023-08-15 22:13:16 +0530

    Auth Login completed, backend remaining

--- frontend/src/components/Auth.js
+++ frontend/src/components/Auth.js
@@ -1,10 +1,83 @@
-import React from 'react'
+import { Box, Button, TextField, Typography } from "@mui/material";
+import React, { useState } from "react";
 
 const Auth = () => {
-  return (
-    <div>Auth</div>
-    // will get displayed only when AppBar in Header position is set to sticky
-  )
-}
+  // to render the inputs of form
+  const [inputs, setInputs] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const [isSignedUp, setIsSignedUp] = useState(false);
 
-export default Auth
+  const handleChange = (e)=>{
+    setInputs((prevState)=>({
+        ...prevState,
+        [e.target.name] : e.target.value,
+    })
+  )}
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    // need this to handle and process form without making entire page getting loaded again
+    console.log(inputs)
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        {/* input fields from material UI */}
+        <Box
+          maxWidth={400}
+          display="flex"
+          flexDirection={"column"}
+          alignItems="center"
+          justifyContent={"center"}
+          boxShadow="10px 10px 20px #ccc"
+          padding={3}
+          margin="auto"
+          marginTop={5}
+          borderRadius={5}
+        >
+          <Typography variant="h4" textAlign="center">
+            {!isSignedUp ? "Login" : "Signup"}
+          </Typography>
+          {isSignedUp && (
+            <TextField name = "name" onChange={handleChange} value={inputs.name} placeholder="Name" margin="normal" />
+          )}
+          <TextField
+            name = "email"
+            onChange={handleChange}
+            value={inputs.email}
+            type={"email"}
+            placeholder="Email"
+            margin="normal"
+          />
+          <TextField
+            name = "password"
+            onChange={handleChange}
+            value={inputs.password}
+            type={"password"}
+            placeholder="Password"
+            margin="normal"
+          />
+          <Button
+            type = "submit"
+            variant="contained"
+            sx={{ borderRadius: 3, marginTop: 3 }}
+            color="warning"
+          >
+            Submit
+          </Button>
+          <Button
+            onClick={() => setIsSignedUp(!isSignedUp)}
+            sx={{ borderRadius: 3 }}
+          >
+            Change to {isSignedUp ? "Login" : "Signup"}
+          </Button>
+        </Box>
+      </form>
+    </div>
+  );
+};
+
+export default Auth;
